f12e161223e04a056e68e40c056be695c54f032a
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-10-09 16:40:26 +0600

    conditional rendering applied

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const Cart = ({ carts, handleRemoveCart }) => {
+    let message;
+    if (carts.length === 0) {
+        message = "Please buy at lease one Item!";
+    }
     return (
         <div>
             <h2>Order Summary{carts.length}</h2>
             {
                 carts.map(cart => <p key={cart._id}>{cart.name}
                     <button onClick={() => handleRemoveCart(cart)}>X</button></p>)
+            }
+            {
+                message
             }
         </div>
     );
