42c6928c26d2a5188507c6af28e186b0709e316f
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-10-09 16:50:41 +0600

    navigation style added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = ({ carts, handleRemoveCart }) => {
     }
     return (
         <div>
-            <h2>Order Summary{carts.length}</h2>
+            <h2>Order Summary</h2>
             {
                 carts.map(cart => <p key={cart._id}>{cart.name}
                     <button onClick={() => handleRemoveCart(cart)}>X</button></p>)
